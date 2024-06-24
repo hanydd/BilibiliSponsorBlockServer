650d3d2e1e871fcdfad0610ba74e7b2724f67841
HanYaodong <hanyaodongcn@gmail.com>
2024-06-24 11:17:17 +0800

    Handle get port video not found

--- src/routes/getPortVideo.ts
+++ src/routes/getPortVideo.ts
@@ -4,19 +4,34 @@ import { PortVideo } from "../types/portVideo.model";
 import { QueryCacher } from "../utils/queryCacher";
 import { portVideoCacheKey } from "../utils/redisKeys";
 import { VideoID } from "../types/segments.model";
+import { validate } from "../utils/bilibiliID";
+import { Logger } from "../utils/logger";
 
 export async function getPortVideo(req: Request, res: Response): Promise<Response> {
-    const videoID = req.query.videoID as VideoID;
+    const bvID = req.query.videoID as VideoID;
 
-    function getPortVideoDB(): Promise<PortVideo> {
+    // validate parameters
+    if (!validate(bvID)) {
+        return res.status(400).send("无效BV号");
+    }
+
+    // get data and cache in redis
+    function getPortVideoDB(): Promise<PortVideo[]> {
         return db.prepare(
-            "get",
+            "all",
             `SELECT "bvID", "ytbID", "UUID", "votes", "locked", "hidden", "biliDuration", "ytbDuration" FROM "portVideo"
             WHERE "bvID" = ? AND "hidden" = 0 AND "votes" > -2`,
-            [videoID]
+            [bvID]
         );
     }
-    const portVideoInfo: PortVideo = await QueryCacher.get(getPortVideoDB, portVideoCacheKey(videoID));
+    const portVideoInfo: PortVideo[] = await QueryCacher.get(getPortVideoDB, portVideoCacheKey(bvID));
 
-    return res.json(portVideoInfo);
+    if (!portVideoInfo || portVideoInfo.length == 0) {
+        return res.sendStatus(404);
+    } else if (portVideoInfo.length >= 1) {
+        // multiple found
+        // TODO: mark the highes vote or latest as the only valid record
+        Logger.error(`Multiple port video matches found for ${bvID}`);
+    }
+    return res.json(portVideoInfo[0]);
 }
