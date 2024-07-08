f28b3f14491de7356559361b45632d6e3814c84d
HanYaodong <hanyaodongcn@gmail.com>
2024-07-08 12:56:12 +0800

    Fix sqlite sha function

--- databases/_upgrade_sponsorTimes_18.sql
+++ databases/_upgrade_sponsorTimes_18.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 
 /* Add hash field */
 ALTER TABLE "lockCategories" ADD "hashedVideoID" TEXT NOT NULL default '';
-UPDATE "lockCategories" SET "hashedVideoID" = sha256("videoID");
+-- UPDATE "lockCategories" SET "hashedVideoID" = sha256("videoID");
 
 UPDATE "config" SET value = 18 WHERE key = 'version';
 
--- databases/_upgrade_sponsorTimes_3.sql
+++ databases/_upgrade_sponsorTimes_3.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 /* hash upgrade test sha256('vid') = '1ff838dc6ca9680d88455341118157d59a055fe6d0e3870f9c002847bebe4663' */
 /* Add hash field */
 ALTER TABLE "sponsorTimes" ADD "hashedVideoID" TEXT NOT NULL default '';
-UPDATE "sponsorTimes" SET "hashedVideoID" = sha256("videoID");
+-- UPDATE "sponsorTimes" SET "hashedVideoID" = sha256("videoID");
 
 /* Bump version in config */
 UPDATE "config" SET value = 3 WHERE key = 'version';
