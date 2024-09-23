19391502c90e2d55ea998c96abf2bfc44155c395
hanyd <hanyd@bsbsb.top>
2024-09-23 16:50:32 +0800

    Update db segment type

--- src/types/segments.model.ts
+++ src/types/segments.model.ts
@@ -1,5 +1,6 @@
 import { HashedValue } from "./hash.model";
 import { SBRecord } from "./lib.model";
+import { portVideoUUID } from "./portVideo.model";
 import { HashedUserID, UserID } from "./user.model";
 
 export type SegmentUUID = string & { __segmentUUIDBrand: unknown };
@@ -16,7 +17,7 @@ export enum ActionType {
     Mute = "mute",
     Chapter = "chapter",
     Full = "full",
-    Poi = "poi"
+    Poi = "poi",
 }
 
 // Uncomment as needed
@@ -54,34 +55,42 @@ export interface Segment {
 export enum Visibility {
     VISIBLE = 0,
     HIDDEN = 1,
-    MORE_HIDDEN = 2
+    MORE_HIDDEN = 2,
 }
 
 export interface DBSegment {
-    category: Category;
-    actionType: ActionType;
+    videoID: VideoID;
     startTime: number;
     endTime: number;
+
+    votes: number;
+    locked: boolean;
     UUID: SegmentUUID;
     userID: UserID;
-    votes: number;
-    views: number;
-    locked: boolean;
-    hidden: HiddenType;
-    required: boolean; // Requested specifically from the client
-    shadowHidden: Visibility;
-    videoID: VideoID;
-    videoDuration: VideoDuration;
-    reputation: number;
-    hashedVideoID: VideoIDHash;
     timeSubmitted: number;
-    userAgent: string;
+    views: number;
+
+    category: Category;
+    actionType: ActionType;
     service: Service;
+
+    videoDuration: VideoDuration;
+    hidden: HiddenType;
+    reputation: number;
+    shadowHidden: Visibility;
+    hashedVideoID: VideoIDHash;
+    userAgent: string;
     description: string;
+
+    ytbID: VideoID;
+    ytbSegmentUUID: SegmentUUID;
+    portUUID: portVideoUUID;
+
+    required: boolean; // Requested specifically from the client
 }
 
 export interface OverlappingSegmentGroup {
-    segments: DBSegment[],
+    segments: DBSegment[];
     votes: number;
     locked: boolean; // Contains a locked segment
     required: boolean; // Requested specifically from the client
@@ -103,19 +112,19 @@ export interface VideoData {
 }
 
 export interface SegmentCache {
-    shadowHiddenSegmentIPs: SBRecord<VideoID, SBRecord<string, Promise<{hashedIP: HashedIP}[] | null>>>,
-    userHashedIP?: HashedIP
+    shadowHiddenSegmentIPs: SBRecord<VideoID, SBRecord<string, Promise<{ hashedIP: HashedIP }[] | null>>>;
+    userHashedIP?: HashedIP;
     userHashedIPPromise?: Promise<HashedIP>;
 }
 
 export interface DBLock {
-    videoID: VideoID,
-    userID: HashedUserID,
-    actionType: ActionType,
-    category: Category,
-    hashedVideoID: VideoIDHash,
-    reason: string,
-    service: Service,
+    videoID: VideoID;
+    userID: HashedUserID;
+    actionType: ActionType;
+    category: Category;
+    hashedVideoID: VideoIDHash;
+    reason: string;
+    service: Service;
 }
 
 export enum SortableFields {
@@ -131,7 +140,7 @@ export enum VoteType {
     Upvote = 1,
     ExtraDownvote = 2,
     Undo = 20,
-    Malicious = 30
+    Malicious = 30,
 }
 
 export enum HiddenType {
