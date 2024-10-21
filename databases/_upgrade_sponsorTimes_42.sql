c844adc2517fc6df9d63df23d87dd35c99c3ec9f
hanyd <hanyd@bsbsb.top>
2024-10-21 17:12:50 +0800

    Fix sqlite drop

--- databases/_upgrade_sponsorTimes_42.sql
+++ databases/_upgrade_sponsorTimes_42.sql
@@ -1,9 +1,11 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS "thumbnails" CASCADE;
+DROP TABLE IF EXISTS "thumbnails" CASCADE; --!sqlite-ignore
+DROP TABLE IF EXISTS "thumbnails";
 DROP TABLE IF EXISTS "thumbnailTimestamps";
 DROP TABLE IF EXISTS "thumbnailVotes";
-DROP TABLE IF EXISTS "titles" CASCADE;
+DROP TABLE IF EXISTS "titles" CASCADE; --!sqlite-ignore
+DROP TABLE IF EXISTS "titles";
 DROP TABLE IF EXISTS "titleVotes";
 DROP TABLE IF EXISTS "ratings";
 
