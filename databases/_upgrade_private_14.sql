ae975b0aae8c961b4c8646860eeb8a62027cf4fc
hanyd <hanyd@bsbsb.top>
2024-11-04 00:11:20 +0800

    Add default cid empty string

--- databases/_upgrade_private_14.sql
+++ databases/_upgrade_private_14.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE "sponsorTimes" ADD "cid" TEXT;
+ALTER TABLE "sponsorTimes" ADD "cid" TEXT NOT NULL DEFAULT '';
 
 UPDATE "config" SET value = 14 WHERE key = 'version';
 
--- databases/_upgrade_sponsorTimes_43.sql
+++ databases/_upgrade_sponsorTimes_43.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE "sponsorTimes" ADD "cid" TEXT;
-ALTER TABLE "portVideo" ADD "cid" TEXT;
-ALTER TABLE "videoInfo" ADD "cid" TEXT;
-ALTER TABLE "lockCategories" ADD "cid" TEXT;
+ALTER TABLE "sponsorTimes" ADD "cid" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "portVideo" ADD "cid" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "videoInfo" ADD "cid" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "lockCategories" ADD "cid" TEXT NOT NULL DEFAULT '';
 
 UPDATE "config" SET value = 43 WHERE key = 'version';
 
