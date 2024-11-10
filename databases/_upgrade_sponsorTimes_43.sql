6cd84b6ee5b6695fd8f9162f7725926be85f7e20
hanyd <hanyd@bsbsb.top>
2024-11-10 16:16:33 +0800

    make cid primary key

--- databases/_upgrade_sponsorTimes_43.sql
+++ databases/_upgrade_sponsorTimes_43.sql
@@ -2,8 +2,23 @@ BEGIN TRANSACTION;
 
 ALTER TABLE "sponsorTimes" ADD "cid" TEXT NOT NULL DEFAULT '';
 ALTER TABLE "portVideo" ADD "cid" TEXT NOT NULL DEFAULT '';
-ALTER TABLE "videoInfo" ADD "cid" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "archivedSponsorTimes" ADD "cid" TEXT NOT NULL DEFAULT '';
 ALTER TABLE "lockCategories" ADD "cid" TEXT NOT NULL DEFAULT '';
+
+
+CREATE TABLE "sqlb_temp_table_43" (
+	"videoID"	TEXT NOT NULL,
+	"cid"	    TEXT NOT NULL DEFAULT '',
+	"channelID"	TEXT NOT NULL,
+	"title"	    TEXT NOT NULL,
+	"published"	NUMERIC NOT NULL,
+	PRIMARY KEY("videoID", "cid")
+);
+
+INSERT INTO sqlb_temp_table_43 SELECT "videoID", '', "channelID", "title", "published" FROM "videoInfo";
+
+DROP TABLE "videoInfo";
+ALTER TABLE sqlb_temp_table_43 RENAME TO "videoInfo";
 
 UPDATE "config" SET value = 43 WHERE key = 'version';
 
