8221a7d59e4d484f0008b595c741d6c46181dbb3
hanyd <hanyd@bsbsb.top>
2024-11-23 12:45:56 +0800

    Remove webhooks

--- src/routes/postWarning.ts
+++ src/routes/postWarning.ts
@@ -5,28 +5,25 @@ import { isUserVIP } from "../utils/isUserVIP";
 import { getHashCache } from "../utils/getHashCache";
 import { HashedUserID, UserID } from "../types/user.model";
 import { config } from "../config";
-import { generateWarningDiscord, warningData, dispatchEvent } from "../utils/webhookUtils";
 import { WarningType } from "../types/warning.model";
 
 type warningEntry = {
-    userID: HashedUserID,
-    issueTime: number,
-    issuerUserID: HashedUserID,
-    enabled: boolean,
-    reason: string
-}
+    userID: HashedUserID;
+    issueTime: number;
+    issuerUserID: HashedUserID;
+    enabled: boolean;
+    reason: string;
+};
 
 function checkExpiredWarning(warning: warningEntry): boolean {
     const MILLISECONDS_IN_HOUR = 3600000;
     const now = Date.now();
-    const expiry =  Math.floor(now - (config.hoursAfterWarningExpires * MILLISECONDS_IN_HOUR));
+    const expiry = Math.floor(now - config.hoursAfterWarningExpires * MILLISECONDS_IN_HOUR);
     return warning.issueTime > expiry && !warning.enabled;
 }
 
-const getUsername = (userID: HashedUserID) => db.prepare("get", `SELECT "userName" FROM "userNames" WHERE "userID" = ?`, [userID], { useReplica: true });
-
 export async function postWarning(req: Request, res: Response): Promise<Response> {
-    if (!req.body.userID) return res.status(400).json({ "message": "Missing parameters" });
+    if (!req.body.userID) return res.status(400).json({ message: "Missing parameters" });
 
     const issuerUserID: HashedUserID = req.body.issuerUserID ? await getHashCache(req.body.issuerUserID as UserID) : null;
     const userID: HashedUserID = issuerUserID ? req.body.userID : await getHashCache(req.body.userID as UserID);
@@ -35,20 +32,24 @@ export async function postWarning(req: Request, res: Response): Promise<Response
     const reason: string = req.body.reason ?? "";
     const type: WarningType = req.body.type ?? WarningType.SponsorBlock;
 
-    if ((!issuerUserID && enabled) || (issuerUserID && !await isUserVIP(issuerUserID))) {
+    if ((!issuerUserID && enabled) || (issuerUserID && !(await isUserVIP(issuerUserID)))) {
         Logger.warn(`Permission violation: User ${issuerUserID} attempted to warn user ${userID}.`);
-        return res.status(403).json({ "message": "Not a VIP" });
+        return res.status(403).json({ message: "Not a VIP" });
     }
 
     let resultStatus = "";
 
     try {
         if (enabled) {
-            const previousWarning = await db.prepare("get", 'SELECT * FROM "warnings" WHERE "userID" = ? AND "issuerUserID" = ? AND "type" = ?', [userID, issuerUserID, type]) as warningEntry;
+            const previousWarning = (await db.prepare(
+                "get",
+                'SELECT * FROM "warnings" WHERE "userID" = ? AND "issuerUserID" = ? AND "type" = ?',
+                [userID, issuerUserID, type]
+            )) as warningEntry;
 
             if (!previousWarning) {
                 if (!reason) {
-                    return res.status(400).json({ "message": "Missing warning reason" });
+                    return res.status(400).json({ message: "Missing warning reason" });
                 }
                 await db.prepare(
                     "run",
@@ -56,12 +57,13 @@ export async function postWarning(req: Request, res: Response): Promise<Response
                     [userID, issueTime, issuerUserID, reason, type]
                 );
                 resultStatus = "issued to";
-            // check if warning is still within issue time and warning is not enabled
-            } else if (checkExpiredWarning(previousWarning) ) {
-                await db.prepare(
-                    "run", 'UPDATE "warnings" SET "enabled" = 1, "reason" = ? WHERE "userID" = ? AND "issueTime" = ?',
-                    [reason, userID, previousWarning.issueTime]
-                );
+                // check if warning is still within issue time and warning is not enabled
+            } else if (checkExpiredWarning(previousWarning)) {
+                await db.prepare("run", 'UPDATE "warnings" SET "enabled" = 1, "reason" = ? WHERE "userID" = ? AND "issueTime" = ?', [
+                    reason,
+                    userID,
+                    previousWarning.issueTime,
+                ]);
                 resultStatus = "re-enabled for";
             } else {
                 return res.sendStatus(409);
@@ -69,27 +71,6 @@ export async function postWarning(req: Request, res: Response): Promise<Response
         } else {
             await db.prepare("run", 'UPDATE "warnings" SET "enabled" = 0 WHERE "userID" = ? AND "type" = ?', [userID, type]);
             resultStatus = "removed from";
-        }
-
-        const targetUsername = await getUsername(userID) ?? null;
-        const issuerUsername = await getUsername(issuerUserID) ?? null;
-        const webhookData = {
-            target: {
-                userID,
-                username: targetUsername
-            },
-            issuer: {
-                userID: issuerUserID,
-                username: issuerUsername
-            },
-            reason
-        } as warningData;
-
-        try {
-            const warning = generateWarningDiscord(webhookData);
-            dispatchEvent("warning", warning);
-        } catch /* istanbul ignore next */ (err) {
-            Logger.error(`Error sending warning to Discord ${err}`);
         }
 
         return res.status(200).json({
