077f2541876f48707ab579cf47d4ef70a4b97f57
hanyd <hanyd@bsbsb.top>
2024-12-06 14:16:29 +0800

    add running key cut queue

--- src/service/api/ApiRateQueue.ts
+++ src/service/api/ApiRateQueue.ts
@@ -13,7 +13,8 @@ export class ApiQueue {
     private rate: number;
     private checkFrequency: number;
 
-    private isRunning: boolean;
+    private runningKey: string;
+    private runningList: QueueItem[];
     private isWaiting: boolean;
     private nextRequest: number;
     private queue: KeyedQueue<QueueItem>;
@@ -22,7 +23,7 @@ export class ApiQueue {
         this.rate = rate;
         this.checkFrequency = checkFrequency;
 
-        this.isRunning = false;
+        this.resetRuningStatus();
         this.isWaiting = false;
         this.nextRequest = 0;
         this.queue = new KeyedQueue();
@@ -32,6 +33,10 @@ export class ApiQueue {
 
     public callApi<T>(key: string, func: () => Promise<T>): Promise<T> {
         return new Promise((resolve, reject) => {
+            if (this.runningKey === key) {
+                this.runningList.push({ func, resolve, reject } as QueueItem);
+                return;
+            }
             this.queue.push(key, { func, resolve, reject } as QueueItem);
             this.checkToCallApi();
         });
@@ -41,20 +46,22 @@ export class ApiQueue {
         if (this.queue.length() == 0) {
             return;
         }
-        if (!this.isRunning && performance.now() >= this.nextRequest) {
-            this.isRunning = true;
+        if (!this.runningKey && performance.now() >= this.nextRequest) {
+            this.runningKey = "tmp";
             const { key, list } = this.queue.shift();
+            this.runningKey = key;
+            this.runningList = list;
 
             if (list.length === 0) {
-                this.isRunning = false;
+                this.resetRuningStatus();
             } else {
-                list[0]
+                this.runningList[0]
                     .func()
-                    .then((result) => list.forEach((item) => item.resolve(result)))
-                    .catch((reason) => list.forEach((item) => item.reject(reason)))
+                    .then((result) => this.runningList.forEach((item) => item.resolve(result)))
+                    .catch((reason) => this.runningList.forEach((item) => item.reject(reason)))
                     .finally(() => {
                         this.nextRequest = performance.now() + this.rate;
-                        this.isRunning = false;
+                        this.resetRuningStatus();
                     });
             }
         }
@@ -65,5 +72,10 @@ export class ApiQueue {
                 this.checkToCallApi();
             }, this.checkFrequency);
         }
+    }
+
+    private resetRuningStatus() {
+        this.runningKey = null;
+        this.runningList = null;
     }
 }
